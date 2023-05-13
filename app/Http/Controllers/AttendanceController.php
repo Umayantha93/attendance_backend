ae36c707ea3e58c1f0fea821259304bcab1f5172
Umayantha93 <umayantha93@gmail.com>
2023-05-13 13:39:03 +0530

    implement controller functions and design attendance service

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -3,20 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Src\AppHumanResources\Attendance\Application\AttendanceService;
 
 class AttendanceController extends Controller
 {
     //
     public function postFile(Request $request)
     {
-        
+        $request->validate([
+            'file' => 'required|mimes:csv,txt'
+        ]);
+    
+        if($request)
+        {
+            $attendance = AttendanceService::postAttendance($request);
+        }
+
+        return response()->json([
+            'message' => "data imported"
+        ]);
         
     }
 
     public function getFile()
     {
 
+        $attendance = AttendanceService::getAttendance();
 
+        return response()->json($attendance);
         
     }
 }
---
+++ app/src/AppHumanResources/Attendance/Application/AttendanceService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\src\AppHumanResources\Attendance\Application;
+
+// use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Src\AppHumanResources\Attendance\Domain\Attendance;
+use Excel;
+
+class AttendanceService
+{
+    public static function postAttendance($request)
+    {
+        $file = file($request->file->getRealPath());
+        $data = array_slice($file, 1);
+
+        $parts = (array_chunk($data, 5000));
+
+        foreach($parts as $index=>$part){
+            $filename = resource_path('pending-files/'.date('y-m-d-H-i-s').$index.'.csv');
+
+            file_put_contents($filename, $part);
+        }
+
+        session()->flash('status', 'queued for importing');
+
+        $path = resource_path('pending-files/*.csv');
+
+        $g = glob($path);
+
+        foreach (array_slice($g, 0, 1) as $file){
+
+            $data = array_map('str_getcsv', file($file));
+            // dd($data);
+            foreach ($data as $row) {
+                Attendance::create([
+                    'emp_id' => $row[0],
+                    'check_in' => $row[1],
+                    'check_out' => $row[2],
+                    'schedule_id' => $row[3],
+                ]);
+            }
+
+            unlink($file);
+        }
+    }
+
+
+    public static function getAttendance()
+    {
+            
+            $attendance = DB::table('attendance')
+            ->join('employees', 'employees.id', '=', 'attendance.emp_id',)
+            ->select('attendance.*', 'employees.name', DB::raw('TIMEDIFF(check_out, check_in) as working_hours'))
+            ->get();
+            
+        return $attendance;
+    }
+}
